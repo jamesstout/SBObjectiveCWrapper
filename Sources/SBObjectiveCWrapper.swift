005d8d285ca7187a71735ffdca043b5629ef1450
James Stout <stoutyhk@gmail.com>
2018-10-20 19:50:33 +0800

    chore(lint): whitespace

--- Sources/SBObjectiveCWrapper.swift
+++ Sources/SBObjectiveCWrapper.swift
@@ -22,48 +22,48 @@ protocol Loggable: class {
 extension SwiftyBeaver: Loggable {}
 
 @objc open class SBObjectiveCWrapper: NSObject {
-    
+
     @objc class func _setLogClassForTesting(_ logClass: AnyObject) {
         if let logC = logClass as? Loggable.Type {
             self.logClass = logC
         }
     }
-    
+
     static var logClass: Loggable.Type = SwiftyBeaver.self
-    
+
     @objc open class func logVerbose(_ message: String, filePath: String, function: String, line: Int) {
-        
+
         logClass.verbose(message, filePath, self.convertFunction(function), line: line, context: nil)
-        
+
     }
-    
+
     @objc open class func logDebug(_ message: String, filePath: String, function: String, line: Int) {
-        
+
         logClass.debug(message, filePath, self.convertFunction(function), line: line, context: nil)
-        
+
     }
-    
+
     @objc open class func logInfo(_ message: String, filePath: String, function: String, line: Int) {
-        
+
         logClass.info(message, filePath, self.convertFunction(function), line: line, context: nil)
-        
+
     }
-    
+
     @objc open class func logWarning(_ message: String, filePath: String, function: String, line: Int) {
-        
+
         logClass.warning(message, filePath, self.convertFunction(function), line: line, context: nil)
-        
+
     }
-    
+
     @objc open class func logError(_ message: String, filePath: String, function: String, line: Int) {
-        
+
         logClass.error(message, filePath, self.convertFunction(function), line: line, context: nil)
-        
+
     }
-    
+
     @objc open class func convertFunction(_ function: String) -> String {
         var strippedFunction = function
-        
+
         if let match = function.range(of: "\\w+:*[\\w*:*]*]", options: .regularExpression) {
             let endIndex = function.index(before: match.upperBound)
             let functionParts = function[match.lowerBound..<endIndex].components(separatedBy: ":")
@@ -82,7 +82,7 @@ extension SwiftyBeaver: Loggable {}
                 }
             }
             if functionParts.count > 1 { strippedFunction += ")" } else if functionParts.count == 1 { strippedFunction += "()" }
-            
+
         }
         return strippedFunction
     }
